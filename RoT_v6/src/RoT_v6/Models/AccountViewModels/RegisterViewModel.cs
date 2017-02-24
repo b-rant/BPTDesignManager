6183e9f5010a08454e014a49532deaebb8e120c9
Saeyangc <saecharles@gmail.com>
2017-02-24 13:45:55 -0800

    DB_simple

--- RoT_v6/src/RoT_v6/Models/AccountViewModels/RegisterViewModel.cs
+++ RoT_v6/src/RoT_v6/Models/AccountViewModels/RegisterViewModel.cs
@@ -45,46 +45,45 @@ namespace RoT_v6.Models.AccountViewModels
         public List<SelectListItem> Roles { get; set; }
         public string Role { get; set; }
 
-        /*public RegisterViewModel()
+        public RegisterViewModel()
         {
             Roles = new List<SelectListItem>();
+            //Roles.Add(new SelectListItem()
+            //{
+            //    Value = "1",
+            //    Text = "Admin"
+            //});
+            //Roles.Add(new SelectListItem()
+            //{
+            //    Value = "2",
+            //    Text = "ShopManager"
+            //});
+            //Roles.Add(new SelectListItem()
+            //{
+            //    Value = "3",
+            //    Text = "Purchaser"
+            //});
+            //Roles.Add(new SelectListItem()
+            //{
+            //    Value = "4",
+            //    Text = "Employee"
+            //});
 
-            Roles.Add(new SelectListItem()
-            {
-                Value = "1",
-                Text = "Admin"
-            });
-            Roles.Add(new SelectListItem()
-            {
-                Value = "2",
-                Text = "ShopManager"
-            });
-            Roles.Add(new SelectListItem()
-            {
-                Value = "3",
-                Text = "Purchaser"
-            });
-            Roles.Add(new SelectListItem()
-            {
-                Value = "4",
-                Text = "Employee"
-            });
+        }
 
-        }*/
-
-       /* public void getRoles(ApplicationDbContext _context)
-           {
-               var roles = from r in _context.identityRole select r;
-               var listRole = roles.ToList();
-               foreach (var Data in listRole)
-               {
-                   Roles.Add(new SelectListItem()
-                   {
-                       Value = Data.Id,
-                       Text = Data.Name
-                 });
-              }
-           }*/
+        //public void getRoles(ApplicationDbContext _context)
+        //{
+        //    var roles = from r in _context.identityRole select r;
+        //    var listRole = roles.ToList();
+        //    foreach (var Data in listRole)
+        //    {
+        //        Roles.Add(new SelectListItem()
+        //        {
+        //            Value = Data.Id,
+        //            Text = Data.Name
+        //        });
+        //    }
+        //}
     }
 }
 
