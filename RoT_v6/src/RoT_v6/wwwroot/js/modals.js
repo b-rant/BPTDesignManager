4750563db7769d0d73854778984460b58e6cef03
Brant DenHerder <brant4474@gmail.com>
2017-03-04 12:27:23 -0800

    Export to CSV, small fixes
    added correct user restrictions
    fixed total bars being to large and now turn red over 100%
    Shows users name instead of email on home bar

--- RoT_v6/src/RoT_v6/wwwroot/js/modals.js
+++ RoT_v6/src/RoT_v6/wwwroot/js/modals.js
@@ -8,7 +8,7 @@ dataUrl = function (mainPage, action, jobId) {
             doModal(result);
         }
     });
-}
+};
 
 function doModal(result) {
     html = '<div id="dynamicModal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="confirm-modal" aria-hidden="true">';
@@ -30,4 +30,108 @@ function doModal(result) {
         $(this).remove();
     });
 
+}
+
+function exportCSV_JobDetails(json) {
+
+
+
+
+
+
+
+    //var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(json));
+    var dlAnchorElem = document.getElementById('downloadAnchorElem');
+    dlAnchorElem.setAttribute("href", dataStr);
+    dlAnchorElem.setAttribute("download", "scene.json");
+    dlAnchorElem.click();
+
+}
+
+
+
+function JSONToCSVConvertor(JSONData, ReportTitle) {
+    //If JSONData is not an object then JSON.parse will parse the JSON string in an Object
+    //var arrData = typeof JSONData != 'object' ? JSON.parse(JSONData) : JSONData;
+
+    var Purchases = JSONData.Purchases;
+    var Job = JSONData.Job;
+    var ActiveTasks = JSONData.ActiveTasks;
+    var CompletedTasks = JSONData.CompletedTasks;
+
+    var CSV = '';
+    
+    CSV += objectToString(Job, "Job Details");
+    CSV += objectToString(ActiveTasks, "Active Tasks");
+    CSV += objectToString(CompletedTasks, "Completed Tasks");
+    CSV += objectToString(Purchases, "Purchases");
+
+    if (CSV == '') {
+        alert("Invalid data");
+        return;
+    }
+
+    //Generate a file name
+    var fileName = "ExportedJob_";
+    //this will remove the blank-spaces from the title and replace it with an underscore
+    fileName += ReportTitle.replace(/ /g, "_");
+
+    //Initialize file format you want csv or xls
+    var uri = 'data:text/csv;charset=utf-8,' + escape(CSV);   
+
+    //this trick will generate a temp <a /> tag
+    var link = document.createElement("a");
+    link.href = uri;
+
+    //set the visibility hidden so it will not effect on your web-layout
+    link.style = "visibility:hidden";
+    link.download = fileName + ".csv";
+
+    //this part will append the anchor tag and remove it after automatic click
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
+
+function objectToString(JSONData, ReportTitle) {
+    var arrData = [0];
+    if(ReportTitle == "Job Details"){
+        arrData[0] = typeof JSONData != 'object' ? JSON.parse(JSONData) : JSONData;;
+    } else {
+        arrData = typeof JSONData != 'object' ? JSON.parse(JSONData) : JSONData;
+    }
+    var CSV = '';
+    
+    CSV += '\r\n\n' + ReportTitle + '\r\n\n';
+
+    var row = "";
+
+    //This loop will extract the label from 1st index of on array
+    for (var index in arrData[0]) {
+
+        //Now convert each value to string and comma-seprated
+        row += index + ',';
+    }
+
+    row = row.slice(0, -1);
+
+    //append Label row with line break
+    CSV += row + '\r\n';
+
+    //1st loop is to extract each row
+    for (var i = 0; i < arrData.length; i++) {
+        var row = "";
+
+        //2nd loop will extract each column and convert it in string comma-seprated
+        for (var index in arrData[i]) {
+            row += '"' + arrData[i][index] + '",';
+        }
+
+        row.slice(0, row.length - 1);
+
+        //add a line break after each row
+        CSV += row + '\r\n';
+    }
+
+    return CSV;
 }
